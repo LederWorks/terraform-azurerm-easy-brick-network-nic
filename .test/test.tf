479c0b734110da35bd04124010375d207287e8d8
Bence Bánó <18106157+Ledermayer@users.noreply.github.com>
2023-09-15 17:33:24 +0200

    fix test

--- .test/test.tf
+++ .test/test.tf
@@ -9,19 +9,19 @@ resource "azurerm_virtual_network" "vnet-tde3-it-terratest-network-nic" {
 resource "azurerm_subnet" "snet-tde3-it-terratest-network-nic001" {
   name = "snet-tde3-it-terratest-network-nic001"
   resource_group_name = azurerm_resource_group.rgrp-tde3-it-terratest-network-nic.name
-  location = azurerm_resource_group.rgrp-tde3-it-terratest-network-nic.location
+  virtual_network_name = azurerm_virtual_network.vnet-tde3-it-terratest-network-nic.name
   address_prefixes = [ "10.0.0.0/24" ]
 }
 resource "azurerm_subnet" "snet-tde3-it-terratest-network-nic002" {
   name = "snet-tde3-it-terratest-network-nic002"
   resource_group_name = azurerm_resource_group.rgrp-tde3-it-terratest-network-nic.name
-  location = azurerm_resource_group.rgrp-tde3-it-terratest-network-nic.location
+  virtual_network_name = azurerm_virtual_network.vnet-tde3-it-terratest-network-nic.name
   address_prefixes = [ "10.0.1.0/24" ]
 }
 resource "azurerm_subnet" "snet-tde3-it-terratest-network-nic003" {
   name = "snet-tde3-it-terratest-network-nic003"
   resource_group_name = azurerm_resource_group.rgrp-tde3-it-terratest-network-nic.name
-  location = azurerm_resource_group.rgrp-tde3-it-terratest-network-nic.location
+  virtual_network_name = azurerm_virtual_network.vnet-tde3-it-terratest-network-nic.name
   address_prefixes = [ "10.0.2.0/24" ]
 }
 
