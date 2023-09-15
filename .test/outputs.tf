11f84ac51f697b88975df62bfb7741a2b49097ae
Bence Bánó <18106157+Ledermayer@users.noreply.github.com>
2023-09-15 21:04:56 +0200

    hmmmm

--- .test/outputs.tf
+++ .test/outputs.tf
@@ -1,14 +1,14 @@
 ################################ Outputs
 ################################ auth
-output "tenant_id" {
-  value = data.azurerm_client_config.current.tenant_id
-}
-output "subscription_id" {
-  value = data.azurerm_client_config.current.subscription_id
-}
-output "client_id" {
-  value = data.azurerm_client_config.current.client_id
-}
+# output "tenant_id" {
+#   value = data.azurerm_client_config.current.tenant_id
+# }
+# output "subscription_id" {
+#   value = data.azurerm_client_config.current.subscription_id
+# }
+# output "client_id" {
+#   value = data.azurerm_client_config.current.client_id
+# }
 
 ################################ rgrp
 output "resource_group_name" {
